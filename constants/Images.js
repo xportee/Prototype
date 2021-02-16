c052fc1953a56c568805d968797d372c4dba775c
syahrizailm <syahrizacio@gmail.com>
2021-02-17 00:32:45 +0900

    Xportee with picture

--- constants/Images.js
+++ constants/Images.js
@@ -1,10 +1,10 @@
-const Onboarding = 'https://images.unsplash.com/photo-1505995433366-e12047f3f144?fit=crop&w=840&q=80';
-const Pro = 'https://images.unsplash.com/photo-1485796826113-174aa68fd81b?fit=crop&w=840&q=80';
+const Onboarding = 'https://source.unsplash.com/kyCNGGKCvyw/840x840';
+const Pro = 'https://source.unsplash.com/kyCNGGKCvyw/840x840';
 const Products = {
   'Accessories': 'https://source.unsplash.com//l1MCA0VyNrk/840x840',
 };
 
-const Profile = 'https://images.unsplash.com/photo-1512529920731-e8abaea917a5?fit=crop&w=840&q=80';
+const Profile = 'https://source.unsplash.com/G_oWb_hcfx8/1800x900';
 const Avatar = 'https://images.unsplash.com/photo-1518725522904-4b3939358342?fit=crop&w=210&q=80';
 
 const Viewed = [
